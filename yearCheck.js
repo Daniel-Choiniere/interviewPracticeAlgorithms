36ccefcafb824572e592901da58b4aad2e7d2aeb
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-07-30 18:55:53 -0400

    changed to function to return instead of log

--- yearCheck.js
+++ yearCheck.js
@@ -1,14 +1,14 @@
 function yearCheck(month, year) {
     let today = new Date();
     let userDate = new Date(year + '-' + month);
-    console.log(today);
-    console.log(userDate);
+    // console.log(today);
+    // console.log(userDate);
 
     let timePassed = today - userDate;
 
     let days = (timePassed / (1000 * 60 * 60 * 24)).toFixed(1);
     let years = days / 365
-    console.log(years.toFixed(2));
+    return years.toFixed(2);
 }
 
 console.log(yearCheck(6, 1985));
