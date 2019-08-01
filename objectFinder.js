6c27c05333f2841cfdfb13a31ce83ce7327a6075
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-08-01 19:37:20 -0400

    got version1.1 working, but having issues implementing 1.2. Where er need to find all objects that contain a matching given key

--- objectFinder.js
+++ objectFinder.js
@@ -1,9 +1,31 @@
-function objectFinder(arr, key) {
+function objectFinder(arr, key, value) {
+    // console.log(arr);
     for (let i=0; i<arr.length; i++) {
-        if (objArray[i].age === key)
+        if (arr[i][key] === value)
         console.log([i]);
     }
 }
 
 const objArray = [{ name: "cliff", age: 32 }, { name: "John", age: 25 }, { name: "Andy", age: 32 }, { name: "Mary", age: 21 }, { name: "Andy", age: 25 }];
-console.log(objectFinder(objArray, 25));
+
+console.log(objectFinder(objArray, "age", 25));
+
+// function objectFinder(arr, key){
+//     for (var i=0; i < arr.length; i++) {
+//         console.log(arr[i]);
+//         if (arr[i][i]=== key) {
+//             // console.log(arr[i]);
+//         }
+//     }
+// }
+
+// function objectFinder(arr, key){
+//     Object.keys(arr).forEach(item => {
+//         console.log(item);
+//       });
+// }
+
+// const objArray = [{ a: "foo", b: "bar" }, { a: "duh", b: 3.7 }, { a: "hi", b: "foo" }];
+
+// console.log(objectFinder(objArray, "foo"));
+
