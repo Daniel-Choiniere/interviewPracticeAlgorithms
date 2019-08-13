ba2144275dc86ce8571f2cced6f7f432bee1012c
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-08-13 19:48:34 -0400

    chnaged up deck arrays to be able to accept queen through ace but passing the array to my value functiom to convert everything to an integer

--- bestHand.js
+++ bestHand.js
@@ -1,4 +1,32 @@
 // VERSION 1.0
+function value(deck) {
+  let hand = [];
+  for (let i = 0; i < deck.length; i++) {
+    switch (deck[i]) {
+      case "J":
+        hand.push(11);
+      case "Q":
+        hand.push(12);
+      case "K":
+        hand.push(13);
+      case "A":
+        hand.push(14);
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+      case 7:
+      case 8:
+      case 9:
+      case 10:
+        hand.push(deck[i]);
+    }
+  }
+  return hand;
+}
+
 function total(hand) {
   let total = 0;
   for (let i = 0; i < hand.length; i++) {
@@ -8,10 +36,12 @@ function total(hand) {
 }
 
 function bestHand(playerOne, playerTwo) {
-  let playerOneTotal = total(playerOne);
-  let playerTwoTotal = total(playerTwo);
-  //   console.log(playerOneTotal);
-  //   console.log(playerTwoTotal);
+  let playerOneTotalValue = value(playerOne);
+  let playerTwoTotalValue = value(playerTwo);
+
+  let playerOneTotal = total(playerOneTotalValue);
+  let playerTwoTotal = total(playerTwoTotalValue);
+
   if (playerOneTotal < playerTwoTotal) {
     console.log("PLAYER TWO WINS!!!");
   } else if (playerOneTotal === playerTwoTotal) {
@@ -21,8 +51,8 @@ function bestHand(playerOne, playerTwo) {
   }
 }
 
-let handOne = [1, 1, 1, 1, 1, 1, 1];
-let handTwo = [1, 1, 1, 1, 1, 1, 1];
+let handOne = [1, 1, 1, 1, 1, 1, 1, "J"];
+// let handTwo = [1, 1, 1, 1, 1, 1, 1];
 let handTwo = [2, 2, 2, 2, 2, 2, 2];
 
 console.log(bestHand(handOne, handTwo));
