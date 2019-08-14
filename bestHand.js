ddb8d0e8fb06da3dfd16ffdcbab8ef18d946497f
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-08-13 20:22:09 -0400

    have version1.1 working of bestHand algorithim. Creates a random deck before getting total of hand

--- bestHand.js
+++ bestHand.js
@@ -1,4 +1,171 @@
-// VERSION 1.0 Create a function to tabulate the hands and pick a winner based on the totals
+// // VERSION 1.0 Create a function to tabulate the hands and pick a winner based on the totals
+// function value(deck) {
+//   let hand = [];
+//   for (let i = 0; i < deck.length; i++) {
+//     switch (deck[i]) {
+//       case "J":
+//         hand.push(11);
+//         break;
+//       case "Q":
+//         hand.push(12);
+//         break;
+//       case "K":
+//         hand.push(13);
+//         break;
+//       case "A":
+//         hand.push(14);
+//         break;
+//       case 1:
+//       case 2:
+//       case 3:
+//       case 4:
+//       case 5:
+//       case 6:
+//       case 7:
+//       case 8:
+//       case 9:
+//       case 10:
+//         hand.push(deck[i]);
+//         break;
+//     }
+//   }
+//   //   console.log(hand);
+//   return hand;
+// }
+
+// function total(hand) {
+//   let total = 0;
+//   for (let i = 0; i < hand.length; i++) {
+//     total += hand[i];
+//   }
+//   console.log(total);
+//   return total;
+// }
+
+// function bestHand(playerOne, playerTwo) {
+//   let playerOneTotalValue = value(playerOne);
+//   let playerTwoTotalValue = value(playerTwo);
+
+//   let playerOneTotal = total(playerOneTotalValue);
+//   let playerTwoTotal = total(playerTwoTotalValue);
+
+//   if (playerOneTotal < playerTwoTotal) {
+//     console.log("PLAYER TWO WINS!!!");
+//   } else if (playerOneTotal === playerTwoTotal) {
+//     console.log("TIE");
+//   } else {
+//     console.log("PLAYER ONE WINS");
+//   }
+// }
+
+// let handOne = [1, 1, 1, 1, 1, 1, 1, "J"];
+// // let handTwo = [1, 1, 1, 1, 1, 1, 1];
+// let handTwo = [2, 2, 2, 2, 2, 2, 2];
+
+// console.log(bestHand(handOne, handTwo));
+
+// VERSION 1.1 Randomize the two hands
+// function randomDeck() {
+//   let deck = ["J", "Q", "K", "A", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//   let newDeck = [];
+//   for (let i = 0; i < 7; i++) {
+//     let randomCard = deck[Math.floor(Math.random() * deck.length)];
+
+//     newDeck.push(randomCard);
+//   }
+
+//   return newDeck;
+// }
+
+// function value(deck) {
+//   let hand = [];
+//   for (let i = 0; i < deck.length; i++) {
+//     switch (deck[i]) {
+//       case "J":
+//         hand.push(11);
+//         break;
+//       case "Q":
+//         hand.push(12);
+//         break;
+//       case "K":
+//         hand.push(13);
+//         break;
+//       case "A":
+//         hand.push(14);
+//         break;
+//       case 1:
+//       case 2:
+//       case 3:
+//       case 4:
+//       case 5:
+//       case 6:
+//       case 7:
+//       case 8:
+//       case 9:
+//       case 10:
+//         hand.push(deck[i]);
+//         break;
+//     }
+//   }
+//   //   console.log(hand);
+//   return hand;
+// }
+
+// function total(hand) {
+//   let total = 0;
+//   for (let i = 0; i < hand.length; i++) {
+//     total += hand[i];
+//   }
+//   return total;
+// }
+
+// function bestHand(playerOne, playerTwo) {
+//   //   console.log(playerOne);
+//   //   console.log(playerTwo);
+//   let playerOneTotalValue = value(playerOne);
+//   let playerTwoTotalValue = value(playerTwo);
+
+//   //   console.log(playerOneTotalValue);
+//   //   console.log(playerTwoTotalValue);
+
+//   let playerOneTotal = total(playerOneTotalValue);
+//   let playerTwoTotal = total(playerTwoTotalValue);
+
+//   console.log("Player one total:", playerOneTotal);
+//   console.log("Player two total:", playerTwoTotal);
+
+//   if (playerOneTotal < playerTwoTotal) {
+//     console.log("PLAYER TWO WINS!!!");
+//   } else if (playerOneTotal === playerTwoTotal) {
+//     console.log("TIE");
+//   } else {
+//     console.log("PLAYER ONE WINS");
+//   }
+// }
+
+// let handOne = randomDeck();
+// // console.log(handOne);
+
+// let handTwo = randomDeck();
+// // console.log(handTwo);
+
+// console.log(bestHand(handOne, handTwo));
+
+// VERSION 1.2
+function randomDeck() {
+  let deck = ["J", "Q", "K", "A", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  let newDeck = [];
+  for (let i = 0; i < 7; i++) {
+    let randomCard = deck[Math.floor(Math.random() * deck.length)];
+
+    newDeck.push(randomCard);
+  }
+
+  return newDeck;
+}
+
 function value(deck) {
   let hand = [];
   for (let i = 0; i < deck.length; i++) {
@@ -38,16 +205,23 @@ function total(hand) {
   for (let i = 0; i < hand.length; i++) {
     total += hand[i];
   }
-  console.log(total);
   return total;
 }
 
 function bestHand(playerOne, playerTwo) {
+  //   console.log(playerOne);
+  //   console.log(playerTwo);
   let playerOneTotalValue = value(playerOne);
   let playerTwoTotalValue = value(playerTwo);
 
+  //   console.log(playerOneTotalValue);
+  //   console.log(playerTwoTotalValue);
+
   let playerOneTotal = total(playerOneTotalValue);
   let playerTwoTotal = total(playerTwoTotalValue);
+
+  console.log("Player one total:", playerOneTotal);
+  console.log("Player two total:", playerTwoTotal);
 
   if (playerOneTotal < playerTwoTotal) {
     console.log("PLAYER TWO WINS!!!");
@@ -58,91 +232,10 @@ function bestHand(playerOne, playerTwo) {
   }
 }
 
-let handOne = [1, 1, 1, 1, 1, 1, 1, "J"];
-// let handTwo = [1, 1, 1, 1, 1, 1, 1];
-let handTwo = [2, 2, 2, 2, 2, 2, 2];
+let handOne = randomDeck();
+// console.log(handOne);
+
+let handTwo = randomDeck();
+// console.log(handTwo);
 
 console.log(bestHand(handOne, handTwo));
-
-// VERSION 1.1 Randomize the two hands
-// function randomDeck() {
-//   let deck = ["J", "Q", "K", "A", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-//   let newDeck = [];
-//   for (let i = 0; i < 7; i++) {
-//     let randomCard = deck[Math.floor(Math.random() * deck.length)];
-
-//     newDeck.push(randomCard);
-//   }
-
-//   return newDeck;
-// }
-
-// function value(deck) {
-//   let hand = [];
-//   for (let i = 0; i < deck.length; i++) {
-//     switch (deck[i]) {
-//       case "J":
-//         hand.push(11);
-//       case "Q":
-//         hand.push(12);
-//       case "K":
-//         hand.push(13);
-//       case "A":
-//         hand.push(14);
-//       case 1:
-//       case 2:
-//       case 3:
-//       case 4:
-//       case 5:
-//       case 6:
-//       case 7:
-//       case 8:
-//       case 9:
-//       case 10:
-//         hand.push(deck[i]);
-//     }
-//   }
-//   console.log(hand);
-//   return hand;
-// }
-
-// function total(hand) {
-//   let total = 0;
-//   for (let i = 0; i < hand.length; i++) {
-//     total += hand[i];
-//   }
-//   return total;
-// }
-
-// function bestHand(playerOne, playerTwo) {
-//   //   console.log(playerOne);
-//   //   console.log(playerTwo);
-//   let playerOneTotalValue = value(playerOne);
-//   let playerTwoTotalValue = value(playerTwo);
-
-//   //   console.log(playerOneTotalValue);
-//   //   console.log(playerTwoTotalValue);
-
-//   let playerOneTotal = total(playerOneTotalValue);
-//   let playerTwoTotal = total(playerTwoTotalValue);
-
-//   //   console.log(playerOneTotal);
-//   //   console.log(playerTwoTotal);
-
-//   if (playerOneTotal < playerTwoTotal) {
-//     console.log("PLAYER TWO WINS!!!");
-//   } else if (playerOneTotal === playerTwoTotal) {
-//     console.log("TIE");
-//   } else {
-//     console.log("PLAYER ONE WINS");
-//   }
-// }
-
-// let handOne = randomDeck();
-// // console.log(handOne);
-
-// let handTwo = randomDeck();
-// // console.log(handTwo);
-
-// console.log(bestHand(handOne, handTwo));
