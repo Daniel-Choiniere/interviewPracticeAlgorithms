a503e062d672856bd88e7f3d6b6d7adcfc827b32
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-08-19 18:28:31 -0400

    rolling all five dice three times

--- yahtzee.js
+++ yahtzee.js
@@ -1,13 +1,25 @@
-const rollDice = () => {
-  const dice = [1, 2, 3, 4, 5, 6];
-  let allRolls = [];
-  let randomRoll;
-  for (let i = 0; i < 5; i++) {
-    randomRoll = dice[Math.floor(Math.random() * dice.length)];
-    allRolls.push(randomRoll);
+const chunkArray = (array, chunk_size) => {
+  var results = [];
+
+  while (array.length) {
+    results.push(array.splice(0, chunk_size));
   }
-  console.log(allRolls);
-  return allRolls;
+  return results;
+};
+
+const rollDice = () => {
+  const die = [1, 2, 3, 4, 5, 6];
+  let allRolls = [];
+  for (let tripleRoll = 0; tripleRoll < 3; tripleRoll++) {
+    for (let rolls = 0; rolls < 5; rolls++) {
+      let randomRoll = die[Math.floor(Math.random() * die.length)];
+      allRolls.push(randomRoll);
+    }
+  }
+  //   console.log(allRolls);
+  const threeRolls = chunkArray(allRolls, 5);
+  console.log(threeRolls);
+  return threeRolls;
 };
 
 const yahtzee = () => {
