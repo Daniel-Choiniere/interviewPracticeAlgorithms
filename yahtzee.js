4701567f0a9b8497b2b2f82698aa063b8025b557
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-08-20 01:13:07 -0400

    yahtzee game has full functionality

--- yahtzee.js
+++ yahtzee.js
@@ -24,12 +24,13 @@ const rollDice = rolls => {
     let randomRoll = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
     allRolls.push(randomRoll);
   }
-  console.log("Our original array is:", allRolls);
   return allRolls;
 };
 
 const yahtzee = () => {
+  // creates our original 5 dice rolled array
   rolledArray = rollDice(5);
+  console.log("Our original array is:", rolledArray);
 
   const rl = readline.createInterface({
     input: process.stdin,
@@ -53,14 +54,33 @@ const yahtzee = () => {
     }
     console.log("Our dice after the second roll are:", rolledArray);
 
-    // checks to see if all the values in the array match, is so we have a Yathzee!!!
-    const allEqual = arr => arr.every(v => v === arr[0]);
-    if (allEqual(rolledArray) === true) {
-      console.log("YAHTZEE!!!");
-    }
+    rl.question("What dice would you like to roll agian? ", answer => {
+      // returns the value(s) we want to reroll
+      let rollAgainDice = rollAgain(answer);
 
-    rl.close();
+      // splices out the chose value(s) from original array
+      removeToBeRolledAgain(rolledArray, rollAgainDice);
+
+      // Re rolls the chosen die/dice
+      let newRolledArray = rollDice(rollAgainDice.length);
+      console.log("On our third roll we get", newRolledArray);
+
+      // adds the newly rolled dice to the original array
+      for (let j = 0; j < newRolledArray.length; j++) {
+        rolledArray.unshift(newRolledArray[j]);
+      }
+      console.log("Our dice after the third roll are:", rolledArray);
+
+      console.log("I'm sorry you are out of turns, please try again!");
+      rl.close();
+    });
   });
+
+  // checks to see if all the values in the array match, is so we have a Yathzee!!!
+  const allEqual = arr => arr.every(v => v === arr[0]);
+  if (allEqual(rolledArray) === true) {
+    console.log("YAHTZEE!!!");
+  }
 };
 
 yahtzee();
