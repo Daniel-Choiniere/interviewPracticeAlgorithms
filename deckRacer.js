28f1fe81d4e69963dc7e45c830c7278c400823b9
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-08-26 20:25:56 -0400

    changed deck clean function to swap with an actual card, not just get a random card without swapping

--- deckRacer.js
+++ deckRacer.js
@@ -29,21 +29,43 @@ function randomHand() {
 }
 
 const checkStartAndEnd = deck => {
-  console.log(deck[0]);
+  //   console.log(deck[51]);
   while (deck[0] === 1 || deck[0] === 2 || deck[0] === 6 || deck[0] === 7) {
-    deck[0] = deck[Math.floor(Math.random() * deck.length)];
+    [deck[0], deck[Math.floor(Math.random() * deck.length)]] = [
+      deck[Math.floor(Math.random() * deck.length)],
+      deck[0]
+    ];
   }
-  console.log(deck);
+  while (deck[1] === 1 || deck[1] === 2 || deck[1] === 6 || deck[1] === 7) {
+    [deck[1], deck[Math.floor(Math.random() * deck.length)]] = [
+      deck[Math.floor(Math.random() * deck.length)],
+      deck[1]
+    ];
+  }
+  while (deck[50] === 1 || deck[50] === 2 || deck[50] === 6 || deck[50] === 7) {
+    [deck[50], deck[Math.floor(Math.random() * deck.length)]] = [
+      deck[Math.floor(Math.random() * deck.length)],
+      deck[50]
+    ];
+  }
+  while (deck[51] === 1 || deck[51] === 2 || deck[51] === 6 || deck[51] === 7) {
+    [deck[51], deck[Math.floor(Math.random() * deck.length)]] = [
+      deck[Math.floor(Math.random() * deck.length)],
+      deck[51]
+    ];
+  }
+  return deck;
 };
 
 const deckRacer = () => {
-  //   console.log(new deck());
-  console.log("The dice roll is: ", rollDice(6));
-
+  // create our deck of 52 cards
   let deck = randomHand();
-
+  // clean up the deck so we don't have values 1, 2, 6, 7 in the first, second, last, or second to last positions
   let cleanedDeck = checkStartAndEnd(deck);
   //   console.log(cleanedDeck);
+
+  let playerOnePosition = -1;
+  let playerTwoPosition = -1;
 };
 
 deckRacer();
